2a29b4c57f6d05188b36ae04d73b0cdd0bdeba43
DatTran <ngugu1253@gmail.com>
2025-09-14 14:44:25 +0700

    Đề Chuyên Tin 9/14/2025

--- B2/b2.py
+++ B2/b2.py
@@ -1,7 +1,26 @@
-x1 = None
-x2 = None
-a = None
+import sys
 
-with open(".//b2.INP", 'r') as f:
-    for l in f.readlines():
-        print(l.strip())
+vt_tho, vt_carot, buoc_moi_lan = None, None, None
+
+try:
+    with open(".\\b2.INP", 'r') as f:
+        giatrinhap = [int(i) for i in f.read().split()]
+        if len(giatrinhap) == 3 and 1 <= giatrinhap[0] <= giatrinhap[1] <= 10 ** 12 and 1 <= giatrinhap[2] <= 10 ** 3:
+            vt_tho, vt_carot, buoc_moi_lan = giatrinhap
+            f.close()
+        else: 
+            print('Nhap Loi')
+            f.close()
+            sys.exit()
+except FileNotFoundError:
+    print('File Khong Tim Thay')
+    sys.exit()
+
+dem = 0
+while vt_carot > vt_tho:
+    vt_tho += buoc_moi_lan
+    dem += 1
+
+with open('.\\b2.OUT', 'w') as f:
+    f.write(str(dem))
+    f.close()
