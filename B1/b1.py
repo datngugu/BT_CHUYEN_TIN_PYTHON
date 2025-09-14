4c20c7d37507934b331200a45de77efb313ebe6f
DatTran <ngugu1253@gmail.com>
2025-09-14 12:23:34 +0700

    Đề chuyên tin 9/14/2025

---
+++ B1/b1.py
@@ -0,0 +1,36 @@
+import sys, math
+
+giatrinhapvao = input('Nhap n, q: ').split(' ')
+daysok = []
+n = int(giatrinhapvao[0])
+q = int(giatrinhapvao[1])
+if n >= 2 and n <= 10**6 and q <= 10**6 and len(giatrinhapvao) == 2:
+    for i in range(1, q + 1):
+        l = int(input('Nhap K: '))
+        if l >= 2 and l <= n:
+            daysok.append(l)
+        else:
+            print('Nhap loi')
+else:
+    print('Nhap loi')
+    sys.exit()
+
+def kt_so_nguyen_to(so):
+    for z in range(2, int(so ** 0.5) + 1):
+        if so % z == 0:
+            return False
+    else:
+        return True
+        
+
+for y in daysok:
+    dem = 0
+    for j in range(2, n + 1):
+        maxUngt = 0
+        for n in range(2, j + 1):
+            if j % n == 0:
+                if kt_so_nguyen_to(n) and n > maxUngt:
+                    maxUngt = n
+        if maxUngt <= y:
+            dem += 1
+    print(dem)
---
+++ B2/b2.py
@@ -0,0 +1,7 @@
+x1 = None
+x2 = None
+a = None
+
+with open(".//b2.INP", 'r') as f:
+    for l in f.readlines():
+        print(l.strip())
