9ea6e14664d7da4f816f8c2d6669e9e7ce39f95c
DatTranDT <ngugu1253@gmail.com>
2025-09-16 16:07:26 +0700

    Đề Chuyên Tin Bài 8 (9/16/1025)

---
+++ B8/b8.py
@@ -0,0 +1,25 @@
+n = 0
+dayso = []
+
+with open(".//b8.INP", 'r') as f:
+    lines = f.readlines()
+    n,dayso = int(lines[0].strip()), [int(giatrinhap) for giatrinhap in lines[1].split()]
+    f.close()
+
+maxUCLN = 0
+
+for i in range(0, n):
+    ucln = 0
+    for j in range(i + 1, n):
+        a = dayso[i]
+        b = dayso[j]
+        while not(a == b):
+            if a > b:
+                a -= b
+            else:
+                b -= a
+        ucln = a
+        if ucln > maxUCLN:
+            maxUCLN = ucln
+
+print(maxUCLN)
